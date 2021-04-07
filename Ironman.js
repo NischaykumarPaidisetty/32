1d729fbf7210eeec30548154e3654374d0e8f950
NischaykumarPaidisetty <75566765+NischaykumarPaidisetty@users.noreply.github.com>
2021-04-07 15:14:19 +0530

    Add files via upload

---
+++ Ironman.js
@@ -0,0 +1,31 @@
+class  Ironman {
+    constructor(x,y,r)
+      {
+          var options = { 
+              density: 1, 
+              frictionAir: 1
+          };
+          this.x=x;
+          this.y=y;
+          this.r=r;
+          this.image=loadImage("Ironman.png");
+          this.body=Bodies.circle(this.x, this.y, (this.r)/2, options);
+          World.add(world, this.body);
+  
+      }
+      
+      display()
+      {
+              
+              var IronmanPos=this.body.position;		
+              push()
+              translate(IronmanPos.x, IronmanPos.y-100);
+              rectMode(CENTER)
+              fill(255,0,255)
+              imageMode(CENTER);
+              image(this.image, 0,0,this.r+150, this.r)
+              pop()
+              
+      }
+  }
+  
---
+++ sketch.js
@@ -0,0 +1,80 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var stone1, stone2, stone3,stone4;
+var ironman,venom,rope,ground;
+
+function preload() {
+  bg = loadImage("gamingbackground1.png");
+}
+
+function setup() {
+  createCanvas(3000, 700);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(600, 600, 1200, 20);
+
+  ironman = new Ironman(400,800,250);
+
+  rope = new Rope(ironman.body, { x: 500, y: 50 });
+  venom = new Venom(1100,550,300);
+
+  stone1 = new stone(900, 100, 70, 70);
+  stone2 = new stone(900, 100, 70, 70);
+  stone3 = new stone(900, 100, 70, 70);
+  stone4 = new stone(900, 100, 70, 70);
+  stone5 = new stone(900, 100, 70, 70);
+  stone6 = new stone(900, 100, 70, 70);
+  stone7 = new stone(900, 100, 70, 70);
+  stone8 = new stone(900, 100, 70, 70);
+  stone9 = new stone(900, 100, 70, 70);
+  stone10 = new stone(800, 100, 70, 70);
+  stone11 = new stone(800, 100, 70, 70);
+  stone12 = new stone(800, 100, 70, 70);
+  stone13 = new stone(800, 100, 70, 70);
+  stone14 = new stone(800, 100, 70, 70);
+  stone15 = new stone(800, 100, 70, 70);
+  stone16 = new stone(800, 100, 70, 70);
+
+
+}
+
+function draw() {
+  background(bg);
+
+  Engine.update(engine);
+  
+  ground.display();
+  stone1.display();
+  stone2.display();
+  stone3.display();
+  stone4.display()
+  stone5.display();
+  stone6.display();
+  stone7.display();
+  stone8.display()
+  stone9.display();
+  stone10.display();
+  stone11.display();
+  stone12.display()
+  stone13.display();
+  stone14.display();
+  stone15.display();
+  stone16.display()
+
+  ironman.display();
+  rope.display();
+  venom.display();
+
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(ironman.body, {x: mouseX , y: mouseY});
+}
+
+
+
